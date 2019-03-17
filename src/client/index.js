727ad36e6b1bf41c8e37f0ee440fa24ebe6ef25b
Max <mgriffith37@gmail.com>
2019-03-17 19:56:42 +1300

    Removed styled components

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import globalStyle from './theme/globalStyle';
 
 ReactDOM.render(<App />, document.getElementById('root'));
